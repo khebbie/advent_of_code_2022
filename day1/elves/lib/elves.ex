3c03d2e8a69ac779c0f0d19f3189e9ecafacf5e7
Klaus Hebsgaard <klaus@hebsgaard.dk>
2022-12-19 15:39:29 +0100

    added error handling provided by ChatGPT

--- day1/elves/lib/elves.ex
+++ day1/elves/lib/elves.ex
@@ -1,4 +1,3 @@
-
 defmodule Elf do
   def calc(calories) do
       calories
@@ -10,16 +9,18 @@ defmodule Elf do
 
 defmodule Elves do
   def import do
-    {:ok, contents} = File.read('/src/elixir/advent_of_code/day1/input_real.txt')
-
-    elves_calories =
-      contents
-      |> String.trim()
-      |> String.split("\n\n", trim: true)
-    results = Enum.map(elves_calories, &Elf.calc/1)
-    IO.inspect Enum.max(results)
-
-
+    case File.read('/src/elixir/advent_of_code/day1/input_real.txt') do
+      {:ok, contents} ->
+        elves_calories =
+          contents
+          |> String.trim()
+          |> String.split("\n\n", trim: true)
+          results = Enum.map(elves_calories, &Elf.calc/1)
+          IO.inspect Enum.max(results)
+        {:error, reason} ->
+            # Handle the error here
+            IO.puts "Error reading file: #{reason}"
+    end
   end
 end
 
