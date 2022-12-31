2c5af48213938cca5920f9d41108315c76d0a862
Klaus Hebsgaard <klaus@hebsgaard.dk>
2022-12-31 12:18:36 +0100

    fixup

--- day4/day4/src/main/scala/Main.scala
+++ day4/day4/src/main/scala/Main.scala
@@ -1,28 +1,26 @@
 import scala.io.Source
 
 object Main extends App {
-  def handleLine(line:String){
-    val Array(range1, range2) = line
+  def handleLine(line: String)={
+    line
       .split(",")
       .map((range: String) => {
         val Array(first, second) = range.split("-")
         Range.inclusive(first.toInt, second.toInt)
       })
+  }
+
+  var fullyContainedCount = 0
+  var overlappedCount = 0
+  val filename = "/src/elixir/advent_of_code/day4/day4/input_real.txt"
+  for (line <- Source.fromFile(filename).getLines) {
+    val Array(range1, range2) = handleLine(line)
     if (range1.containsSlice(range2) || range2.containsSlice(range1)) {
-      println("found")
       fullyContainedCount = fullyContainedCount + 1
     }
     if (range1.intersect(range2).size > 0) {
       overlappedCount = overlappedCount + 1
     }
-
-
-  }
-  var fullyContainedCount = 0
-  var overlappedCount = 0
-  val filename = "/src/elixir/advent_of_code/day4/day4/input_real.txt"
-  for (line <- Source.fromFile(filename).getLines) {
-    handleLine(line)
   }
 
   println(fullyContainedCount)
