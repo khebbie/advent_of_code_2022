3b282e2aac6326aceddd1c4d08a48635d530d888
Klaus Hebsgaard <klaus@hebsgaard.dk>
2022-12-31 11:44:33 +0100

    format

--- day4/day4/src/main/scala/Main.scala
+++ day4/day4/src/main/scala/Main.scala
@@ -5,24 +5,22 @@ object Main extends App {
   var overlappedCount = 0
   val filename = "/src/elixir/advent_of_code/day4/day4/input_real.txt"
   for (line <- Source.fromFile(filename).getLines) {
-      val Array(range1, range2) =line
+    val Array(range1, range2) = line
       .split(",")
       .map((range: String) => {
         val Array(first, second) = range.split("-")
-        Range.inclusive(first.toInt,second.toInt)
+        Range.inclusive(first.toInt, second.toInt)
       })
-      if(range1.containsSlice(range2) || range2.containsSlice(range1)){
-        println("found")
-        fullyContainedCount = fullyContainedCount + 1
-      }
-      if(range1.intersect(range2).size > 0){
-        overlappedCount = overlappedCount + 1
-      }
-
+    if (range1.containsSlice(range2) || range2.containsSlice(range1)) {
+      println("found")
+      fullyContainedCount = fullyContainedCount + 1
+    }
+    if (range1.intersect(range2).size > 0) {
+      overlappedCount = overlappedCount + 1
+    }
 
   }
 
-    println(fullyContainedCount)
-    println(overlappedCount)
+  println(fullyContainedCount)
+  println(overlappedCount)
 }
-
